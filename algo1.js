17a1e6743e44b149fd056c6191e2aae7fea8ce0c
Numissa11 <aline_sinclaire@hotmail.fr>
2020-04-03 15:36:47 +0100

    algo1

--- algo1.js
+++ algo1.js
@@ -10,6 +10,8 @@ In this room, the seats are organized as follows:
 Create a function that returns an array of seats.
 Each item in the array should be in the form: row-seat.
 
+
+
 The final array should look something like this:
 [
     1-1,
@@ -23,3 +25,33 @@ The final array should look something like this:
     26-100
 ]
 */
+function siet(){
+
+    let arrRow = [];
+  
+  for (var i = 1; i <= 26; i++) {
+      arrRow.push(i);
+  }
+  
+  let arrSeat = [];
+  
+  for (var i = 1; i <= 100; i++) {
+     arrSeat.push(i);
+  }
+     let finalArray = [];
+    let temporaryArray = [];
+  
+    for (let i = 0; i < arrRow.length; i++) {
+      for (let j = 0; j < arrSeat.length; j++) {
+        temporaryArray.push(`${arrRow[i]} - ${arrSeat[j]}`)
+      }
+  
+      finalArray.push(temporaryArray);
+      temporaryArray = [];
+    }
+    return finalArray;
+  }
+  
+  console.log(siet());
+
+  
