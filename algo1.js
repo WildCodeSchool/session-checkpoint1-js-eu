6d5f894f67c6a2fd7398287ac01b4f48b590ab12
Filipa Marta <filipa.marta@gmail.com>
2020-04-03 10:39:48 +0100

    task algo1.js completed

--- algo1.js
+++ algo1.js
@@ -23,3 +23,30 @@ The final array should look something like this:
     26-100
 ]
 */
+
+const tickets = () => {
+
+    const rowsArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+    const minSeats = 1;
+    const maxSeats = 100;
+    let seatsArray = [];
+    let cinema = [];
+    
+    //create the array of seats from 1 to 100:
+    for (let i = minSeats; i <= maxSeats; i++) {
+        seatsArray.push(i);
+    }
+    //console.log(seatsArray);
+
+    //create the loop to add 100 seats to each of the 26 rows:
+    for (let i = 0; i < rowsArray.length; i++) {
+        //console.log(rowsArray[i]);
+        for (let j = 0; j < seatsArray.length; j++) {
+            cinema.push(`${rowsArray[i]}-${seatsArray[j]}`);
+        }
+    }
+    //console.log(cinema);
+    return cinema;
+};
+
+tickets();
