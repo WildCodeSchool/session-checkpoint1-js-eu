a654bc1e436590dc24c4cbf6673ba1d24a424f12
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-04-03 10:46:08 +0100

    algo3.js solved

--- algo3.js
+++ algo3.js
@@ -1,10 +1,19 @@
 /*
-Given a number that represents the position of the minute hand on a clock (number of minutes from 0 - 60) write a program that returns the angle that is formed by the minute hand and 12.
+Given a number that represents the position of the minute hand on a clock (number of minutes from 0 - 60)
+ write a program that returns the angle that is formed by the minute hand and 12.
 
-If the user enters a number that is above 60, your program should fail, and log a friendly message to the console, informing the user of their error. 
+If the user enters a number that is above 60, your program should fail, and log a friendly message 
+to the console, informing the user of their error. 
 (Hint: Create an HTML UI to capture input and display the result of running you algorithm)
 */
 
 function minuteAngle(minute) {
-
+  if (minute > 60) {
+    return `Invalid input, try another number of minutes`
+  } else {
+    let minuteAngle = (360 / 60) * Math.abs(minute);
+    return minuteAngle;
+  }
 }
+
+console.log(minuteAngle(30));
